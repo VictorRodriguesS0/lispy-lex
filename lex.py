e090a87f2ef74d47ca706b3935224b45d7f0f791
Victor Rodrigues <victorrsilva17@gmail.com>
2020-10-15 15:22:46 -0300

    Adding Regex Tokens

--- lex.py
+++ lex.py
@@ -12,5 +12,15 @@ def lex(code: str) -> Iterable[Token]:
     Retorna sequência de objetos do tipo token correspondendo à análise léxica
     da string de código fornecida.
     """
+    tokens = [
+        ("LPAR", r"\("),
+        ("RPAR", r"\)"),
+        ("STRING", r"\".*\""),
+        ("NUMBER", r"(\+|\-|)\d+(\.\d*)?"),
+        ("NAME", r"([a-zA-Z_%\+\-]|\.\.\.)[a-zA-Z_0-9\-\>\?\!]*"),
+        ("CHAR", r"#\\[a-zA-Z]*"),
+        ("BOOL", r"#[t|f]"),
+        ("QUOTE", r"\'"),
+    ]
 
     return [Token('INVALIDA', 'valor inválido')]
